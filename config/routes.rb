79596556d06f10a8f46b64ba928554d5a32d04d0
p137 <p137@gmx.com>
2015-09-03 16:45:51 +0200

    Added acquiring the link and processing it. Installed Bootstrap.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'giveaway_helper#new'
+
+  post 'pick' => 'giveaway_helper#pick'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
